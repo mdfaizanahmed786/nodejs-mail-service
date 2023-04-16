718a2d73d101865a30bc30ab758c375912f95c52
Faizan Ahmed <faizanzaid789@gmail.com>
2023-04-16 11:31:01 +0530

    ADDED NEW PORT

--- server/index.js
+++ server/index.js
@@ -285,6 +285,6 @@ app.post("/send",async (req, res) => {
   res.status(200).json({ message: "Email sent!" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
   console.log("Server is running on port 3000");
 });
